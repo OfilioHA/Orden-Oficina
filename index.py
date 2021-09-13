02af3a3b973333e273cfceacb3e77430ad9e4fbf
Ofilio_Herdocia <Herdocia20@gmail.com>
2021-09-13 10:48:49 -0600

    Lectura de .env y variables.

--- index.py
+++ index.py
@@ -1,16 +1,23 @@
 from flask import Flask, render_template;
+from dotenv import load_dotenv;
+import os;
 
+load_dotenv()
+DB_ADDR = os.getenv('DB_ADDR')
+
+print(DB_ADDR);
 app = Flask(__name__);
 
 
 @app.route('/')
 @app.route('/agua')
 @app.route('/tienda')
+@app.route('/fumigacion')
 def index():
     return render_template('index.html');
 
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
-        debug=True
+        debug=True,
     );
